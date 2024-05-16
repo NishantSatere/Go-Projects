ed96da4c831fe0fd195fbb81b37b1b4498b5fec1
NishantSatere <nishantd28satere@gmail.com>
2024-05-17 02:45:10 +0530

    Implemented age-bot

--- Slack-Age-Bot/main.go
+++ Slack-Age-Bot/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shomali11/slacker"
 	"log"
 	"os"
+	"strconv"
 )
 
 func printCommandEvents(analyticChannel <-chan *slacker.CommandEvent) {
@@ -17,7 +18,6 @@ func printCommandEvents(analyticChannel <-chan *slacker.CommandEvent) {
 		fmt.Println(event.Parameters)
 		fmt.Println()
 	}
-
 }
 func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-7115043618226-7112185933221-1iOyCuO6sHLQzd6s3eoKf1yu")
@@ -26,5 +26,28 @@ func main() {
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_USER_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("hello year of birth is <year>", &slacker.CommandDefinition{
+		Description: "Yob calculator",
+		Examples:    []string{"hello year of birth is 1990"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				fmt.Println(err)
+				response.Reply("Invalid year")
+				return
+			}
+			age := 2021 - yob
+			response.Reply(fmt.Sprintf("Your age is %d", age))
+		},
+	})
+
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
